8236d038149dff618e14af12c8355f7804e1c64b
carbajalmatias1621-lab <carbajalmatias1621@gmail.com>
2026-06-04 15:04:13 -0300

    chixd

--- app/obj/Debug/net9.0/app.AssemblyInfo.cs
+++ app/obj/Debug/net9.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ec1b94f17e2fe3972c6301888adfca71ae2cc27")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
